f723a6a7be433da7dd0a04d91c0c4d0df3e146ab
Peiyun <b612starnight@gmail.com>
2025-04-22 23:30:31 +0100

    Initial Commit

---
+++ Tilli/TilliApp.swift
@@ -0,0 +1,20 @@
+//
+//  TilliApp.swift
+//  Tilli
+//
+//  Created by Peiyun on 2025/4/22.
+//
+
+import SwiftUI
+
+@main
+struct TilliApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
